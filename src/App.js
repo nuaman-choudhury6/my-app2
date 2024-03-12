a798ef858b09e79f521fd17746b681bc2f72d6de
nuaman-choudhury6 <nuamanchy66@gmail.com>
2024-03-12 03:15:14 -0700

    task 6 re and controlled

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,Fragment } from 'react';
 import AddUser from './components/Users/AddUser';
 import UserList from './components/Users/UserList';
 
@@ -17,11 +17,12 @@ function App() {
     
   };
   return (
-    <div>
+    <Fragment>
       <AddUser  onAddUser={addUserHandler} />
       <UserList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
 export default App;
+
